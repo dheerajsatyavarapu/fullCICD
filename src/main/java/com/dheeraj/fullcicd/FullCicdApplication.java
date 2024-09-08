60073f512d8d710a6d32a2127123f9b619622917
Dheeraj Satyavarapu <dheerajdheeru126s@gmail.com>
2024-09-08 11:14:35 +0530

    First Commit:

---
+++ src/main/java/com/dheeraj/fullcicd/FullCicdApplication.java
@@ -0,0 +1,13 @@
+package com.dheeraj.fullcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullCicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dheeraj/fullcicd/FullCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dheeraj.fullcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
